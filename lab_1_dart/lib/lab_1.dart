c52a12f15728423b6c339a266b2a5eed586e3528
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-10-07 22:06:10 +0300

    create main() function in lab_1.dart

--- lab_1_dart/lib/lab_1.dart
+++ lab_1_dart/lib/lab_1.dart
@@ -1,3 +1,22 @@
-int calculate() {
-  return 6 * 7;
+import 'package:lab_1/length.dart';
+import 'package:lab_1/null.dart';
+import 'package:lab_1/singleton.dart';
+import 'package:lab_1/snoc.dart';
+
+void main() {
+  // Singleton
+  final listWithArg = singleton(3);
+  print(listWithArg);
+
+  // null
+  print(nullFunc([]));
+  print(nullFunc([2, 'ds']));
+
+  // snoc
+  final list = snoc([2, 'dsa'], 3);
+  print(list);
+
+  // length
+  final lengthOfList = length([1, 2, 3]);
+  print(lengthOfList);
 }
